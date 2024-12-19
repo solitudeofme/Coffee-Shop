f105d89cedd956eb5932bed2fa3f0edbcd8ecb30
Nilou <desnillight@gmail.com>
2024-12-19 10:39:31 +0330

    adding custom styles and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+      boxShadow: {
+        "shadow-normal": "0px 1px 10px rgba(0 ,0 ,0 , 0.05)",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        "pinar-400": "pinar-400",
+        "pinar-700": "pinar-700",
+        iranyekan: "iranyekan regular",
+        iranyekanBold: "iranyekan bold",
+        iranyekanLight: "iranyekan light",
+      },
+    },
   },
   plugins: [],
 };
