cda070c06adef7ab72062495907a4b256d5cd47d
Nilou <desnillight@gmail.com>
2024-12-20 11:33:38 +0330

    adding a custom varient for accessing children through parent

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,5 +30,10 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
 };
