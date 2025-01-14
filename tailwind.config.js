af2d96cc071b7b2ca04e509636816f09f05afa1e
Nilou <desnillight@gmail.com>
2025-01-14 17:54:29 +0330

    putting overlay--invisible in safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/*.{html,js}"],
+  safelist: ["overlay--visible"],
   darkMode: "selector",
   theme: {
     extend: {
